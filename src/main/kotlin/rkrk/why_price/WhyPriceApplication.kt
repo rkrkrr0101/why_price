07efdcd677a4648d120b7e2bb190c0849a9a037b
foko <qkrrudgh0@naver.com>
2024-08-13 10:51:56 +0900

    Initial commit

---
+++ src/main/kotlin/rkrk/why_price/WhyPriceApplication.kt
@@ -0,0 +1,11 @@
+package rkrk.why_price
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WhyPriceApplication
+
+fun main(args: Array<String>) {
+    runApplication<WhyPriceApplication>(*args)
+}
---
+++ src/test/kotlin/rkrk/why_price/WhyPriceApplicationTests.kt
@@ -0,0 +1,13 @@
+package rkrk.why_price
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WhyPriceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
