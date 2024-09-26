3a9fad155df0cf810adc4b2650e23f18a8e53841
foko <qkrrudgh0@naver.com>
2024-09-26 13:04:43 +0900

    refactor:AddMemberKoreanStockDto의 사용하지않는 crno필드를 제거

--- src/main/kotlin/rkrk/whyprice/member/application/port/input/dto/req/AddMemberKoreanStockDto.kt
+++ src/main/kotlin/rkrk/whyprice/member/application/port/input/dto/req/AddMemberKoreanStockDto.kt
@@ -2,6 +2,5 @@ package rkrk.whyprice.member.application.port.input.dto.req
 
 data class AddMemberKoreanStockDto(
     val memberName: String,
-    val stockCrno: String,
     val stockName: String,
 )
