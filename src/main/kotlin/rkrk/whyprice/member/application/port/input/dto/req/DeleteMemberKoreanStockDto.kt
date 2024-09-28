f2d85aa05339b945ffe10473071a6d32dfd92c7f
foko <qkrrudgh0@naver.com>
2024-09-28 14:55:38 +0900

    fix:멤버주식삭제dto의 사용하지않는 필드를 제거함

--- src/main/kotlin/rkrk/whyprice/member/application/port/input/dto/req/DeleteMemberKoreanStockDto.kt
+++ src/main/kotlin/rkrk/whyprice/member/application/port/input/dto/req/DeleteMemberKoreanStockDto.kt
@@ -2,6 +2,6 @@ package rkrk.whyprice.member.application.port.input.dto.req
 
 data class DeleteMemberKoreanStockDto(
     val memberName: String,
-    val stockCrno: String,
+    // val stockCrno: String,
     val stockName: String,
 )
