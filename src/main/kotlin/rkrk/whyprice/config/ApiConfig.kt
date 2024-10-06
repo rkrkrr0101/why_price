833f9def9a6fc4e23f67c04b7eba651030254f1c
foko <qkrrudgh0@naver.com>
2024-10-06 19:25:11 +0900

    chore:주석처리된 부분을 제거함

--- src/main/kotlin/rkrk/whyprice/config/ApiConfig.kt
+++ src/main/kotlin/rkrk/whyprice/config/ApiConfig.kt
@@ -5,29 +5,8 @@ import io.github.cdimascio.dotenv.dotenv
 class ApiConfig private constructor() {
     companion object {
         private val dotenv = dotenv()
-        //        private val properties = Properties()
-//
-//        init {
-//            val inputStream =
-//                this::class.java.classLoader.getResourceAsStream("config.properties")
-//                    ?: throw IllegalArgumentException("config.properties 파일을 찾을 수 없습니다.")
-//            properties.load(inputStream)
-//
-//        }
-
-//        fun getOpenApiKey(): String = properties.getProperty("OPENAPI_KEY")
-//
-//        fun getGptKey(): String = properties.getProperty("GPT_KEY")
-//
-//        fun getPerplexityKey(): String = properties.getProperty("PERPLEXITY_KEY")
-//
-//        fun getKoreaInvKey(): String = properties.getProperty("KOREA_INV_KEY")
-//
-//        fun getKoreaSecretKey(): String = properties.getProperty("KOREA_INV_SECRET")
 
         fun getOpenApiKey(): String = dotenv.get("OPENAPI_KEY")
-
-        fun getGptKey(): String = dotenv.get("GPT_KEY")
 
         fun getPerplexityKey(): String = dotenv.get("PERPLEXITY_KEY")
 
