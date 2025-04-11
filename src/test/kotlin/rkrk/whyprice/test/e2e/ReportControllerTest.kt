15e2b61622fb779dc735453f80be2e300ddbda92
foko <qkrrudgh0@naver.com>
2025-04-12 06:42:30 +0900

    test:레포트컨트롤러테스트의 테스트 변경

--- src/test/kotlin/rkrk/whyprice/test/e2e/ReportControllerTest.kt
+++ src/test/kotlin/rkrk/whyprice/test/e2e/ReportControllerTest.kt
@@ -3,6 +3,7 @@ package rkrk.whyprice.test.e2e
 import kotlinx.coroutines.test.runTest
 import org.hamcrest.Matchers.hasItem
 import org.hamcrest.Matchers.hasSize
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -17,7 +18,10 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPat
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.request
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.transaction.annotation.Transactional
+import rkrk.whyprice.report.adapter.out.persistence.hotstockreport.HotStockReport
+import rkrk.whyprice.report.adapter.out.persistence.hotstockreport.HotStockReportJpaRepository
 import rkrk.whyprice.report.application.port.input.dto.res.ResponseReportDto
+import rkrk.whyprice.report.domain.Report
 import rkrk.whyprice.share.Result
 import rkrk.whyprice.tool.TestConstant
 import rkrk.whyprice.tool.config.TestConfig
@@ -31,51 +35,49 @@ class ReportControllerTest
     @Autowired
     constructor(
         val mvc: MockMvc,
+        private val hotStockReportJpaRepository: HotStockReportJpaRepository
     ) {
-        @Test
-        @DisplayName("특정주식의 레포트를 출력할수있다")
-        fun getStockReport() {
-            runTest {
-                val mvcResult =
-                    mvc
-                        .perform(
-                            MockMvcRequestBuilders
-                                .get("/api/report/stock?stockName=삼성전자")
-                                .contentType(MediaType.APPLICATION_JSON),
-                        ).andExpect(status().isOk)
-                        .andExpect(request().asyncStarted())
-                        .andExpect(
-                            request()
-                                .asyncResult(
-                                    Result(
-                                        ResponseReportDto(
-                                            "삼성전자 report",
-                                            CustomDateTimeMock(TestConstant.TEST_CURRENT_TIME).getNow(),
-                                        ),
-                                    ),
-                                ),
-                        ).andReturn()
-            }
-        }
+    @BeforeEach
+    fun init(){
+        createTestHotStocks()
+    }
 
         @Test
         @DisplayName("거래량상위주식의 레포트를 출력할수있다")
         fun getHighStockReport() {
             runTest {
-                val mvcResult =
                     mvc
                         .perform(
                             MockMvcRequestBuilders
                                 .get("/api/report/stock/high")
                                 .contentType(MediaType.APPLICATION_JSON),
-                        ).andExpect(request().asyncStarted())
-                        .andReturn()
+                        ).andExpect(status().isOk)
+                        .andExpect(jsonPath("$.data", hasSize<Any>(10)))
+                        .andExpect(jsonPath("$.data[*].report", hasItem("1등주식 report")))
 
-                mvc
-                    .perform(asyncDispatch(mvcResult))
-                    .andExpect(status().isOk)
-                    .andExpect(jsonPath("$.data", hasSize<Any>(10)))
-                    .andExpect(jsonPath("$.data[*].report", hasItem("LG전자 report")))
             }
         }
+
+    private fun createTestHotStocks() {
+        hotStockSave("1등주식")
+        hotStockSave("2등주식")
+        hotStockSave("3등주식")
+        hotStockSave("4등주식")
+        hotStockSave("5등주식")
+        hotStockSave("6등주식")
+        hotStockSave("7등주식")
+        hotStockSave("8등주식")
+        hotStockSave("9등주식")
+        hotStockSave("10등주식")
+    }
+
+    private fun hotStockSave(assetName:String){
+        hotStockReportJpaRepository.save<HotStockReport>(
+            HotStockReport(Report(
+                assetName,
+                "$assetName report",
+                CustomDateTimeMock(TestConstant.TEST_CURRENT_TIME).getNow())
+            )
+        )
+    }
     }
